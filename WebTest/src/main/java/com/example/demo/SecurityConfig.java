a6ed5879a24b127a789b1806b516f9aff804e936
sho1131 <sh.katagiri@gmail.com>
2023-09-14 17:10:27 +0900

    WebTest20230914_newadd

---
+++ WebTest/src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+/*
+@EnableWebSecurity
+public class SecurityConfig {
+	  
+		@Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+
+	    @Override
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.inMemoryAuthentication()
+	            .withUser("admin")
+	            .password(passwordEncoder().encode("admin"))
+	            .roles("ADMIN");
+	    }
+}
+*/
